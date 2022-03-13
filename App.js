17e375a0b6095545d085e398c65519a70e8de14b
Huan <hphan298@gmail.com>
2022-03-13 19:22:39 +0700

    1

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import { Text, StyleSheet, View } from 'react-native';
+import React, { Component } from 'react';
+import  FontAwesome5  from 'react-native-vector-icons/FontAwesome5';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>asdasdsaasdasd</Text>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
